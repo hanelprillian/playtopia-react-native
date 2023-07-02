a4bed9e5ad4f3e503b324aaafde4d5848c50a1df
Hanel <hanelp97@gmail.com>
2023-07-03 00:42:59 +0700

    statistics bar homepage

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -8,9 +8,32 @@ import {
   View,
 } from 'react-native';
 import Text from '../components/Text';
-import { positional } from 'yargs';
+import LinearGradient from 'react-native-linear-gradient';
 
 function NotificationButton() {
+  const Style = StyleSheet.create({
+    notificationButton: {
+      backgroundColor: '#FFF',
+      width: 32,
+      height: 32,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 100,
+    },
+    notificationBadge: {
+      backgroundColor: '#FF2E29',
+      width: 8,
+      height: 8,
+      borderColor: '#FFF',
+      borderWidth: 2,
+      borderStyle: 'solid',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      borderRadius: 100,
+    },
+  });
+
   return (
     <TouchableOpacity style={Style.notificationButton}>
       <Image
@@ -23,6 +46,22 @@ function NotificationButton() {
 }
 
 function Header() {
+  const Style = StyleSheet.create({
+    header: {
+      paddingTop: 50,
+      padding: 16,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      height: 225,
+    },
+    headerGreetingText: {
+      fontSize: 14,
+      color: '#005474',
+      borderBottomLeftRadius: 16,
+      borderBottomRightRadius: 16,
+    },
+  });
+
   return (
     <ImageBackground
       style={Style.header}
@@ -35,9 +74,168 @@ function Header() {
         </Text>
       </View>
       <View>
-        <NotificationButton/>
+        <NotificationButton />
       </View>
     </ImageBackground>
+  );
+}
+
+function Content() {
+  const Style = StyleSheet.create({
+    contentContainer: {
+      padding: 16,
+    },
+    content: {
+      marginTop: -78,
+      backgroundColor: '#FFF',
+      borderRadius: 24,
+      shadowColor: 'rgba(0, 0, 0, 0.1)',
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 1,
+      shadowRadius: 40,
+      padding: 16,
+    },
+  });
+
+  return (
+    <View style={Style.contentContainer}>
+      <View style={Style.content}>
+        <Statistics />
+      </View>
+    </View>
+  );
+}
+
+function Statistics() {
+  const Style = StyleSheet.create({
+    statistics: {
+      flexDirection: 'row',
+    },
+    statisticsExpWrapper: {
+      width: '50%',
+      paddingTop: 6,
+      paddingBottom: 10,
+      paddingLeft: 12,
+      paddingRight: 12,
+      borderTopLeftRadius: 12,
+      borderBottomLeftRadius: 12,
+    },
+    statisticsExpText: {
+      fontSize: 10,
+      color: '#005474',
+      marginBottom: 8,
+    },
+    statisticsExpBar: {
+      width: '100%',
+      height: 6,
+      borderRadius: 8,
+      backgroundColor: '#5CD2FF',
+      position: 'relative',
+    },
+    statisticsExpBarFilled: {
+      borderRadius: 8,
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      bottom: 0,
+    },
+    statisticsExpBarFilledIcon: {
+      width: 12,
+      height: 11,
+      position: 'absolute',
+      top: -3,
+      right: -3,
+      objectFit: 'contain',
+    },
+
+    statisticsMilesWrapper: {
+      width: '50%',
+      paddingLeft: 12,
+      paddingRight: 12,
+      borderTopRightRadius: 12,
+      borderBottomRightRadius: 12,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    statisticsMilesTextContainer: {
+      flex: 1,
+    },
+    statisticsMilesText: {
+      fontSize: 10,
+      color: '#710043',
+    },
+    statisticsMilesIcon: {
+      width: 24,
+      height: 24,
+      fontSize: 10,
+      color: '#710043',
+    },
+    statisticsMilesMoreIcon: {
+      width: 16,
+      height: 16,
+    },
+  });
+
+  return (
+    <View style={Style.statistics}>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={['#E1F4FB', '#8ADFFF']}
+        style={Style.statisticsExpWrapper}>
+        <TouchableOpacity>
+          <Text style={Style.statisticsExpText}>
+            <Text style={{...Style.statisticsExpText, fontWeight: 'bold'}}>
+              295 XP{' '}
+            </Text>
+            lagi jadi playlover
+          </Text>
+          <View style={Style.statisticsExpBar}>
+            <LinearGradient
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 1}}
+              colors={['#2EC6FF', '#0BA1D9']}
+              style={{...Style.statisticsExpBarFilled, width: '34%'}}>
+              <Image
+                style={Style.statisticsExpBarFilledIcon}
+                source={require('../assets/icons/love-pink.png')}
+              />
+            </LinearGradient>
+          </View>
+        </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={['#FFB7E2', '#FFE5F4']}
+        style={Style.statisticsMilesWrapper}>
+        <TouchableOpacity
+          style={{flexDirection: 'row', gap: 8, alignItems: 'center'}}>
+          <Image
+            style={Style.statisticsMilesIcon}
+            source={require('../assets/icons/miles-badge.png')}
+          />
+          <View style={Style.statisticsMilesTextContainer}>
+            <Text style={Style.statisticsMilesText}>PLAY MILES</Text>
+            <Text
+              style={{
+                ...Style.statisticsMilesText,
+                fontSize: 12,
+                fontWeight: 'bold',
+              }}>
+              50000
+            </Text>
+          </View>
+          <Image
+            style={Style.statisticsMilesMoreIcon}
+            source={require('../assets/icons/chevron-right-pink.png')}
+          />
+        </TouchableOpacity>
+      </LinearGradient>
+    </View>
   );
 }
 
@@ -50,45 +248,7 @@ export default function HomeScreen() {
         translucent={true}
       />
       <Header />
+      <Content />
     </ScrollView>
   );
 }
-
-const Style = StyleSheet.create({
-  container: {
-    // backgroundColor: 'red',
-  },
-  header: {
-    paddingTop: 50,
-    padding: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: 225,
-  },
-  headerGreetingText: {
-    fontSize: 14,
-    color: '#005474',
-    borderBottomLeftRadius: 16,
-    borderBottomRightRadius: 16,
-  },
-  notificationButton: {
-    backgroundColor: '#FFF',
-    width: 32,
-    height: 32,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-  },
-  notificationBadge: {
-    backgroundColor: '#FF2E29',
-    width: 8,
-    height: 8,
-    borderColor: '#FFF',
-    borderWidth: 2,
-    borderStyle: 'solid',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    borderRadius: 100,
-  },
-});
